d424e970fac9b19cc221cf757a4409ef175229cc
Daniel Patterson <dan24patt@gmail.com>
2016-03-08 22:34:15 +0000

    Split sentences too big for Chrome into chunks
    This should fully work around Chrome's cut off by splitting speech
    chunks to be 250 characters or less (unless it is a single word).

--- background.js
+++ background.js
@@ -1,15 +1,51 @@
 'use strict';
 
+// Chrome only seems able to say a small number of words for a single call to
+// the speech API. Over the limit, it cuts out and will not work until
+//
+//   chrome.tts.stop()
+//
+// is called. This number of characters seems to be below the limit.
+const MAX_SPEECH_LENGTH_IN_CHARACTERS = 250
+
 console.log('Starting background script')
 
+chrome.runtime.onMessage.addListener(request => {
+  console.log(request)
+  if (request.say)
+    say(request.say)
+})
+
 function say(text) {
-  text.split(/[\.,;]/).forEach(textChunk => {
+  speakableChunks(text).forEach(textChunk => {
     chrome.tts.speak(textChunk, { lang: 'en-GB', enqueue: true })
   })
 }
 
-chrome.runtime.onMessage.addListener(function(request) {
-  console.log(request)
-  if (request.say)
-    say(request.say)
-});
+function speakableChunks(text) {
+  return text.split(/[\.,;]/)
+    .map(asChunksSmallEnoughForChrome)
+    .reduce((all, next) => all.concat(next))
+}
+
+function asChunksSmallEnoughForChrome(text) {
+  if (!tooLongForChrome(text))
+    return [text]
+  return reduce([], text.split(' '))((sentences, word) => {
+    return sentences.concat(chromeSizedSentences(sentences.pop() || '', word))
+  })
+}
+
+function chromeSizedSentences(sentence, word) {
+  let sentenceAndWord = `${sentence} ${word}`
+  return tooLongForChrome(sentenceAndWord) ? [sentence, word] : [sentenceAndWord]
+}
+
+function tooLongForChrome(text) {
+  return text.length > MAX_SPEECH_LENGTH_IN_CHARACTERS
+}
+
+function reduce(initial, array) {
+  let fullArray = [initial, ...array]
+  return Array.prototype.reduce.bind(fullArray)
+}
