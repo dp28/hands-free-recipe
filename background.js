5cf8ef0c70fe8cceae680720a79dd82c28efad3e
Daniel Patterson <dan24patt@gmail.com>
2016-03-08 00:39:38 +0000

    Read BBC Good Food recipe methods on page load

--- background.js
+++ background.js
@@ -3,9 +3,8 @@
 console.log('Starting background script')
 
 function say(text) {
-  chrome.tts.speak(text, { lang: 'en-GB' })
+  chrome.tts.speak(text, { lang: 'en-GB', enqueue: true })
 }
-
 
 chrome.runtime.onMessage.addListener(function(request) {
   console.log(request)
--- read.js
+++ read.js
@@ -1,9 +1,17 @@
 'use strict';
 
 function say(text) {
+  console.log('Saying', text)
   chrome.runtime.sendMessage({ say: text })
 }
 
-say('loaded')
+function forEachNode(nodes) {
+  return Array.prototype.forEach.bind(nodes)
+}
+
+var nodes = document.querySelector('ol').querySelectorAll('li')
+
+console.log('Reading', nodes.length, 'nodes')
+forEachNode(nodes)(node => say(node.textContent))
 
 
