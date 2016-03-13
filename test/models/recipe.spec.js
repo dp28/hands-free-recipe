70644593c2a5dacbc323ff2bb4bc3fcdaf3745f9
Daniel Patterson <dan24patt@gmail.com>
2016-03-13 13:47:15 +0000

    Add linting that happens before compilation
    To enforce consistent code style.

--- test/models/recipe.spec.js
+++ test/models/recipe.spec.js
@@ -6,23 +6,23 @@ describe('Recipe', function() {
 
   describe('.ingredients', function() {
     beforeEach(function () {
-      this.ingredients = ['2 onions', '1 pig']
-      this.recipe = new Recipe(this.ingredients, [])
+      this.ingredients = ['2 onions', '1 pig'];
+      this.recipe = new Recipe(this.ingredients, []);
     });
 
     it('returns the ingredients passed in to create the Recipe', function() {
       expect(this.recipe.ingredients).to.deep.equal(this.ingredients);
-    })
+    });
   });
 
   describe('.instructions', function() {
     beforeEach(function () {
-      this.instructions = ['preheat oven', 'put pig in oven for 4 hours']
-      this.recipe = new Recipe([], this.instructions)
+      this.instructions = ['preheat oven', 'put pig in oven for 4 hours'];
+      this.recipe = new Recipe([], this.instructions);
     });
 
     it('returns the instructions passed in to create the Recipe', function() {
       expect(this.recipe.instructions).to.deep.equal(this.instructions);
-    })
+    });
   });
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 module.exports = {
   entry: {
-    "recipe-controller": "./src/recipe-controller.js"
+    "recipe-controller": "./src/controllers/recipe-controller.js"
   },
   output: {
     path: "./dist",
     filename: "[name].js"
   },
   module: {
+    preLoaders: [
+      { test: /\.js$/, loader: "eslint-loader", exclude: /node_modules/ }
+    ],
     loaders: [
-        { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-        { test: /\.jade$/, exclude: /node_modules/, loader: "jade-loader" }
+        { test: /\.js$/, loader: "babel-loader", exclude: /node_modules/ },
+        { test: /\.jade$/, loader: "jade-loader", exclude: /node_modules/ }
     ]
   }
 };
